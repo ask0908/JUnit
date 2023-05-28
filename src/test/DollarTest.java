9fa966cbde08c4f39ed0a9e4e673109ddf9a11ea
ask0908 <askdfjaslk@naver.com>
2023-05-29 00:52:56 +0900

    2장까지

--- src/test/DollarTest.java
+++ src/test/DollarTest.java
@@ -1,2 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * 테스트가 실패하면 assertEquals() 밑에 컴파일 에러처럼 빨간 선이 생김
+ */
 public class DollarTest {
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
+        product = five.times(3);
+        assertEquals(15, product.amount);
+    }
 }
