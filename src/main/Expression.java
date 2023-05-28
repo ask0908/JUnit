76f8473b9e2cd4e5817e3e7dabe13292c34732fd
ask0908 <askdfjaslk@naver.com>
2023-05-29 00:16:51 +0900

    처음부터 시작

--- src/main/Expression.java
+++
@@ -1,8 +0,0 @@
-package main;
-
-/* 클래스로 만들어도 되지만 인터페이스가 상대적으로 가볍다 */
-public interface Expression {
-    Money reduce(Bank bank, String to);
-    Expression plus(Expression addend);
-    Expression times(int multiplier);
-}
